03c0ce14cb14ba58c11b4a6c8d665e88fcecb226
Kerem Demirturk <kerem@sufle.io>
2021-04-10 16:41:01 +0300

    main page updated

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function HomePage() {
-    return <div className="text-purple-500 bg-blue-100">Welcome to Next.js!</div>
+    return (
+    <div className="site-container">
+      <div className="space-y-4">
+        <h1 className="site-text-container text-indigo-400">
+          Kerem Demirtürk
+        </h1>
+
+        <p>
+          Şu anda Sufle şirketinde Cloud and Platform Engineer olarak çalışmaktayım. Bu Blog'da  genel olarak AWS konseptleri, Linux ve Siber Güvenlik ve sürekli takılınabilecek sorunların çözümü üzerine yazılarımı yazmayı düşünüyorum.
+        </p>
+
+
+    </div>
+    </div>)
   }
   
   export default HomePage
