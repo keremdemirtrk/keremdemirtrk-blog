db7cc5aad9d478ed75fa0403019a018188e99919
Kerem Demirturk <kerem@sufle.io>
2021-04-10 22:19:12 +0300

    blog add page upload and some fixes on themes

---
+++ components/mdx-components.js
@@ -0,0 +1,5 @@
+import Header from './header'
+
+export const mdxComponents = {
+   // p: (props) => <p className="text-indigo-800" {...props} />,
+}
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
 function HomePage() {
     return (
-    <div className="site-container">
-      <div className="space-y-4">
-        <h1 className="site-text-container text-indigo-400">
-          Kerem Demirtürk
-        </h1>
+    <>
+      <div className="site-container">
+        <div className="space-y-4">
+          <h1 className="site-text-container text-indigo-400">
+            Kerem Demirtürk
+          </h1>
 
-        <p>
-          Şu anda Sufle şirketinde Cloud and Platform Engineer olarak çalışmaktayım. Bu Blog'da  genel olarak AWS konseptleri, Linux ve Siber Güvenlik ve sürekli takılınabilecek sorunların çözümü üzerine yazılarımı yazmayı düşünüyorum.
-        </p>
+          <p className="text-justify">
+            Şu anda Sufle şirketinde Cloud and Platform Engineer olarak çalışmaktayım. Bu Blog'da  genel olarak AWS konseptleri, Linux, Siber Güvenlik ve sürekli takılınabilecek sorunların çözümü üzerine yazılarımı yazmayı düşünüyorum.
+          </p>
+        </div> 
+      </div>
 
+      <div className="site-xl-container mt-10">
+        <img src="/kd-pp.jpg" alt="" />
+      </div>
 
-    </div>
-    </div>)
+      </>
+      )
   }
   
   export default HomePage
